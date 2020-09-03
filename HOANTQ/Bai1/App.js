05f105cd10718b9c9c60e6b6e7ffc320559b7401
hoantq19 <hoantq19@gmail.com>
2020-09-04 05:15:05 +0700

    moi nhat

--- HOANTQ/Bai1/App.js
+++ HOANTQ/Bai1/App.js
@@ -13,9 +13,11 @@ import {
   View,
   Text,
   Image,
+  _Image,
 } from 'react-native';
 import {
 } from 'react-native/Libraries/NewAppScreen';
+
 
 const App = () => {
   return (
@@ -25,9 +27,7 @@ const App = () => {
         <View
           style={styles.v_setting}>
           <Image
-
-            source={require('./img/img_girl.jpg')}
-
+            source={require('./img/ic_setting.png')}
             style={styles.img_setting} />
         </View>
       </View>
@@ -44,36 +44,36 @@ const App = () => {
           <Text style={styles.text_body}>Annalbel, 22</Text>
           <Text>From Bắc Kinh, China</Text>
         </View>
-        <View
-          style={styles.action_close}>
-          <View
-            style={styles.v_close}>
-            <Image
-              source={require('./img/ic_close.png')}
-              style={styles.img_close} />
-          </View>
+        <View style={styles.action_close}>
+
+          {_arrayClose(require('./img/ic_close.png'))}
+          {_arrayClose(require('./img/ic_heart.png'))}
+
         </View>
       </View>
       <View
         style={styles.story}>
         <Text style={styles.text_story}>Suggested Stories</Text>
-        <View style={styles.story_block}>
-          <Image source={require('./img/ic_plus.png')} style={styles.img_story} />
-          <Text>Add Story</Text>
+        <View style={{ flexDirection: 'row' }}>
+          {_bodyplus(require('./img/ic_plus.png'), 'Add Story')}
+          {_bodygirl(require('./img/img_ly.jpg'), 'Ly')}
+          {_bodygirl(require('./img/img_nguyet.jpg'), 'Nguyệt')}
+          {_bodygirl(require('./img/img_trinh.jpg'), 'Trinh')}
+          {_bodygirl(require('./img/img_hang.jpg'), 'Hằng')}
         </View>
       </View>
-      <View style={styles.head_footer}>
-        <View styles={styles.footer}>
-          <Image
-            source={require('./img/ic_home.png')}
-            style={styles.img_footer} />
-        </View>
+
+      <View style={styles.footer}>
+        {_footerBlock(require('./img/ic_home.png'))}
+        {_footerBlock(require('./img/ic_tv.png'))}
+        {_footerBlock(require('./img/ic_people.png'))}
+        {_footerBlock(require('./img/ic_heart.png'))}
+        {_footerBlock(require('./img/ic_alarm.png'))}
+        {_footerBlock(require('./img/ic_menu.png'))}
       </View>
     </View>
   )
-
 };
-
 _funcBlock = (img, label) => {
   return (
     <View style={styles.v_action_block}>
@@ -81,6 +81,36 @@ _funcBlock = (img, label) => {
       <Text
         style={styles.profile}>{label}</Text>
     </View>
+  )
+}
+_arrayClose = (img) => {
+  return (
+    <View style={styles.v_close}>
+      <Image
+        source={img}
+        style={styles.img_close} />
+    </View>
+  )
+};
+_bodyplus = (img, label) => {
+  return (
+    <View style={styles.story_block}>
+      <Image source={img} style={styles.img_story} />
+      <Text>{label}</Text>
+    </View>
+  )
+}
+_bodygirl = (img, label) => {
+  return (
+    <View style={styles.story_block}>
+      <Image source={img} style={styles.img_story2} />
+      <Text>{label}</Text>
+    </View>
+  )
+}
+_footerBlock = (img) => {
+  return (
+    <Image source={img} style={styles.img_footer} />
   )
 };
 
@@ -99,8 +129,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     flex: 1
   },
-  v_setting:
-  {
+  v_setting: {
     backgroundColor: '#E6E6FA',
     width: 32,
     height: 32,
@@ -108,19 +137,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  img_setting:
-  {
+  img_setting: {
     width: 24,
     height: 24
   },
-  action_block:
-  {
+  action_block: {
     flexDirection: 'row',
     margin: 20,
-
   },
-  v_action_block:
-  {
+  v_action_block: {
     flexDirection: 'row',
     backgroundColor: '#e4e5ea',
     justifyContent: "center",
@@ -129,20 +154,17 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginHorizontal: 4
   },
-  img_user:
-  {
+  img_user: {
     width: 18,
     height: 16,
     marginHorizontal: 6
   },
-  profile:
-  {
+  profile: {
     fontSize: 18,
     fontWeight: 'bold',
     marginRight: 5
   },
-  title_block:
-  {
+  title_block: {
     marginLeft: 20,
     width: 370,
     height: 440,
@@ -150,39 +172,34 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     elevation: 13
   },
-  v_body:
-  {
+  v_body: {
     padding: 16,
     marginLeft: 8
   },
-  text_body:
-  {
+  text_body: {
     fontWeight: 'bold',
     fontSize: 24,
   },
-  img_girl:
-  {
+  img_girl: {
     width: 370,
     height: 300,
     marginTop: 38,
   },
-  action_close:
-  {
+  action_close: {
     flexDirection: 'row',
-    position: 'absolute'
+    position: 'absolute',
+    marginTop: 310,
+    marginLeft: 240,
+    elevation:20
   },
-  v_close:
-  {
+  v_close: {
     backgroundColor: 'white',
     width: 50,
     height: 50,
     borderRadius: 100,
-    marginTop: 310,
-    marginLeft: 250,
-    elevation: 8
+    marginLeft: 6
   },
-  img_close:
-  {
+  img_close: {
     width: 35,
     height: 35,
     marginHorizontal: 8,
@@ -193,46 +210,48 @@ const styles = StyleSheet.create({
     borderTopWidth: 0.5,
     borderBottomWidth: 0.5,
   },
-  text_story:
-  {
+  text_story: {
     marginTop: 6,
     fontSize: 18,
     fontWeight: 'bold'
   },
-  story_block:
-  {
+  story_block: {
     width: 70,
     height: 70,
     backgroundColor: '#e4e5ea',
     borderRadius: 100,
     borderWidth: 3,
-    margin: 6,
+    margin: 5,
     borderColor: '#819F',
     marginBottom: 26,
+    alignItems: 'center'
   },
-  img_story:
-  {
+  img_story: {
     width: 30,
     height: 30,
     margin: 17.5,
     marginBottom: 20,
     justifyContent: "center"
   },
-  head_footer:
+  img_story2:
   {
+    width: 60,
+    height: 60,
+    marginTop: 1.5,
+    borderRadius: 100,
+    marginBottom: 7
 
-    borderTopWidth: 1
   },
-
   footer: {
+    borderTopWidth: 1,
     flexDirection: 'row',
+    justifyContent: 'space-around',
     marginHorizontal: 8
   },
-  img_footer:
-  {
+  img_footer: {
     padding: 16,
     margin: 8,
-    marginLeft: 16
+    marginLeft: 4
   }
 });
 
