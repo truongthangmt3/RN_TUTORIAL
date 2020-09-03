96c1d6b3782e6877004c56f71ec9cf8b98a957a8
Ha_Trung <trungoetze@gmail.com>
2020-09-03 16:42:30 +0700

    no message

--- TrungHD/HomeWork1/index.js
+++ TrungHD/HomeWork1/index.js
@@ -6,4 +6,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/HomeWork1';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('HomeWork1', () => App);
