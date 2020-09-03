2bf1dc3ac96f645aea8cf89da7aea5e25f46b055
hoantq19 <hoantq19@gmail.com>
2020-09-03 20:12:53 +0700

    remore node

--- HOANTQ/Bai1/App.js
+++ HOANTQ/Bai1/App.js
@@ -25,7 +25,7 @@ const App = () => {
         <View
           style={styles.v_setting}>
           <Image
-            source={require('./img/ic_setting.png')}
+            source={require('./img/img_girl.jpg')}
             style={styles.img_setting} />
         </View>
       </View>
