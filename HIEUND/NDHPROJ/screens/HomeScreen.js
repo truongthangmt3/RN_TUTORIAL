7869e8bdd52868b514c37ce10476fa2d8034c5c0
hieu2nd <hieua1cma@gmail.com>
2020-09-18 20:38:12 +0700

    update 18/9

--- HIEUND/NDHPROJ/screens/HomeScreen.js
+++ HIEUND/NDHPROJ/screens/HomeScreen.js
@@ -8,6 +8,8 @@ import {
   FlatList,
 } from 'react-native';
 import mockData from '.././mockData.json';
+import axios from 'axios';
+import Loading from '../src/components/Loading';
 
 export default class HomeScreen extends Component {
   state = {
@@ -15,22 +17,49 @@ export default class HomeScreen extends Component {
     isError: false,
     data: {},
   };
-  componentDidMount() {
-    // setTimeout(() => {
-    this.setState({
-      isLoading: false,
-      isError: false,
-      data: mockData.homeData.data.listpost,
-    });
-    // }, 500);
-  }
+  componentDidMount = async () => {
+    // goi api va cho du lieu tra ve
+    //cach co dien
+    // fetch('http://3.0.209.176/api/GetHome')
+    //   .then((response) => response.json())
+    //   .then((res) => {
+    //     alert(JSON.stringify(res));
+    //   });
+    //cach hien dai tu ES6 dung async va await (thay doi ham thanh async)
+    try {
+      const response = await axios.get('http://3.0.209.176/api/GetHome');
+      const jsonResponse = response.data;
+      // alert(JSON.stringify(jsonResponse));
+      this.setState({
+        isLoading: true,
+        isError: false,
+        data: jsonResponse.data.listpost,
+      });
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        isError: true,
+        data: {},
+      });
+    }
+  };
+  // componentDidMount() {
+  //   // setTimeout(() => {
+  //   this.setState({
+  //     isLoading: false,
+  //     isError: false,
+  //     data: mockData.homeData.data.listpost,
+  //   });
+  //   // }, 500);
+  // }
   render() {
     const {isLoading, isError, data} = this.state;
     if (isLoading) {
       return (
-        <View style={styles.container}>
-          <ActivityIndicator color="red" />
-        </View>
+        // <View style={styles.container}>
+        //   <ActivityIndicator color="red" />
+        // </View>
+        <Loading />
       );
     }
     if (isError) {
@@ -43,6 +72,7 @@ export default class HomeScreen extends Component {
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
+          // horizontal={true}
           style={styles.list_post}
           data={this.state.data}
           renderItem={({item, index}) => {
