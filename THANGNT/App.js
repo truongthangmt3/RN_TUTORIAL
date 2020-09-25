adba9684e854a4785cd3d526a3aae2483718fb99
KAKASHI <truongthangmt3@gmail.com>
2020-09-25 20:09:04 +0700

    Navigation

--- THANGNT/App.js
+++ THANGNT/App.js
@@ -7,37 +7,15 @@
  */
 
 import React, { Component } from 'react';
-import {
-  View,
-  SafeAreaView,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import img from './assets/imagesAsset';
-import CustomButton from './src/components/CustomButton'
-import HomeScreen from './src/screens/HomeScreen.js'
-import UserScreen from './src/screens/UserSreen.js'
-import HomeHookScreen from './src/screens/HomeHookScreen'
-import data from './mockData.json'
-import HookHomeScreen from './src/screens/HookHomeScreen';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
+import AppNavigator from './src/navigation/AppNavigator'
+import NavigationUtil from './src/navigation/NavigationUtil';
 class App extends Component {
   render() {
     return (
-      <HomeScreen />
+      <AppNavigator
+        ref={ref => NavigationUtil.setTopLevelNavigator(ref)}
+      />
     );
   }
 };
-
-const AppNavigator = createStackNavigator({
-  home: HomeScreen,
-  user: UserScreen
-}, {
-  headerMode: 'none',
-})
-
-export default createAppContainer(AppNavigator);
+export default App;
