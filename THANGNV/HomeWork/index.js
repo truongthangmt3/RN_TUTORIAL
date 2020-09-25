facaad30f4d72708e9c2266aac14eda90c3a8cf4
thangnv <thang.120598@gmail.com>
2020-09-25 18:20:55 +0700

    homework

--- THANGNV/HomeWork/index.js
+++ THANGNV/HomeWork/index.js
@@ -2,10 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-import giaodien from './apps/screen/giaodien.js'
-import HomeScreen from './apps/screen/HomeScreen'
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
