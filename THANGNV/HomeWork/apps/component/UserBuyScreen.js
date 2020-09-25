d314a411969e1dfcaa82d99cc1017349e8a151e3
thangnv <thang.120598@gmail.com>
2020-09-25 18:23:34 +0700

    done

---
+++ THANGNV/HomeWork/apps/component/UserBuyScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export class UserBuyScreen extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Thong tin mua </Text>
+      </View>
+    );
+  }
+}
+
+export default UserBuyScreen;
