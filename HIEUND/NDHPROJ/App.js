92d19af06590e1fb67a5ce9b9eacaad9ec57013f
hieu2nd <hieua1cma@gmail.com>
2020-09-30 14:38:56 +0700

    add navigation

--- HIEUND/NDHPROJ/App.js
+++ HIEUND/NDHPROJ/App.js
@@ -7,33 +7,15 @@
  */
 
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
-import CustomButtons from './src/components/CustomButtons';
-import Stories from './src/components/Stories';
-import HomeScreen from './screens/HomeScreen';
-import HomeHookScreen from './screens/HomeHookScreen';
-import UserScreen from './screens/UserScreen';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import AppNavigator from './src/navigation/AppNavigator';
+import NavigationUtil from './src/navigation/NavigationUtil';
 
 class App extends Component {
   render() {
-    return <HomeScreen />;
+    return (
+      <AppNavigator ref={(ref) => NavigationUtil.setTopLevelNavigator(ref)} />
+    );
   }
 }
-const AppNavigator = createStackNavigator(
-  {
-    home: HomeScreen,
-    user: UserScreen,
-  },
-  {headerMode: 'none'},
-);
-export default createAppContainer(AppNavigator);
+
+export default App;
---
+++ HIEUND/NDHPROJ/utils/Constants.js
@@ -0,0 +1,10 @@
+const SCREEN_ROUTER = {
+  SPLASH: 'splash',
+  AUTH: 'auth',
+  LOGIN: 'login',
+  USER: 'user',
+  CUSTOMER: 'customer',
+  NOTIFY: 'notify',
+  HOME: 'home',
+};
+export {SCREEN_ROUTER};
