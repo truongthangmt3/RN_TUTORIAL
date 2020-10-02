e7356aa93a07400d8fba83b8a608f09499e5c312
thanhdat68 <datnguyendinh6886>
2020-10-02 19:54:18 +0700

    add base

--- DATND/project/index.js
+++ DATND/project/index.js
@@ -4,8 +4,11 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
+import AppCopy from './AppCopy';
 import { name as appName } from './app.json';
 
 import HomeScreen from './src/sceens/HomeScreen'
-
-AppRegistry.registerComponent(appName, () => HomeScreen);
+import DatingScreen from './src/sceens/DatingScreen'
+import HookHomeScreen from './src/sceens/HookHomeScreen'
+import Login from './src/sceens/LoginScreen'
+AppRegistry.registerComponent(appName, () => App);
