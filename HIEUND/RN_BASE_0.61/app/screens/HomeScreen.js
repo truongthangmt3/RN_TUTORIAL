2c33088f41b1deaa13a7307a84d62e76a1923704
hieu2nd <hieua1cma@gmail.com>
2020-10-05 11:53:26 +0700

    update 5/10

--- HIEUND/RN_BASE_0.61/app/screens/HomeScreen.js
+++ HIEUND/RN_BASE_0.61/app/screens/HomeScreen.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { requestHomeData } from "@api";
+import Loading from "@component/Loading";
 export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
+  state = {
+    isLoading: true,
+    isError: false,
+    name: "",
+    data: {}
+  };
+  componentDidMount = async () => {
+    try {
+      const response = await requestHomeData();
+      const jsonResponse = response.data;
+      this.setState({
+        isLoading: true,
+        isError: false,
+        data: jsonResponse.data
+      });
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        isError: true,
+        data: {}
+      });
     }
-
-    render() {
-        return (
-            <View style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <Text>RNCA Home </Text>
-            </View>
-        );
+  };
+  render() {
+    const { isLoading, isError, data, name } = this.state;
+    if (isLoading) {
+      return <Loading />;
     }
+    if (isError) {
+      return (
+        <View style={styles.container}>
+          <Text>FAILED</Text>
+        </View>
+      );
+    }
+  }
 }
