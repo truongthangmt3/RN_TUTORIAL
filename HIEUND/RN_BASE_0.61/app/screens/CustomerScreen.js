75379fec2820555247b8b0f86bc3f40a1d26a9c6
hieu2nd <hieua1cma@gmail.com>
2020-10-06 16:32:24 +0700

    update toi di mua si screens to rnbase

---
+++ HIEUND/RN_BASE_0.61/app/screens/CustomerScreen.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Dimensions,
+  ScrollView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import { Header } from "react-native-elements";
+import RightHeaderCustom from "@component/RightHeaderCustom";
+import GoodsInfo from "@component/GoodsInfo";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+export default class CustomerScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header
+          containerStyle={{
+            backgroundColor: "#69AAFF",
+            justifyContent: "space-around"
+          }}
+          placement="left"
+          leftComponent={{
+            text: "Khách hàng quan tâm",
+            style: { color: "#fff", fontSize: 20 }
+          }}
+          rightComponent={<RightHeaderCustom />}
+        />
+        <ScrollView style={{ flex: 1, backgroundColor: "#F5F6F8" }}>
+          <GoodsInfo />
+          <GoodsInfo />
+          <GoodsInfo />
+          <GoodsInfo />
+          <GoodsInfo />
+        </ScrollView>
+      </View>
+    );
+  }
+}
