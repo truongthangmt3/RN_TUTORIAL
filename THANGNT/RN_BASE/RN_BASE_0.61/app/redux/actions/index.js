1c5bb6ad4bb28cff58fa02dd5094e4d4037e9a1f
KAKASHI <truongthangmt3@gmail.com>
2020-10-15 20:35:20 +0700

    b2

--- THANGNT/RN_BASE/RN_BASE_0.61/app/redux/actions/index.js
+++ THANGNT/RN_BASE/RN_BASE_0.61/app/redux/actions/index.js
@@ -1,9 +1,16 @@
-import {
-  GET_USER,
-} from "./type";
+import { GET_USER, GET_HOME, CALCULATION } from './type';
 
-export const getUserInfo = (data) => ({
+export const getUserInfo = data => ({
   type: GET_USER,
   payload: data
 });
 
+export const getHome = data => ({
+  type: GET_HOME,
+  payload: data
+});
+
+export const calculation = data => ({
+  type: CALCULATION,
+  payload: data
+});
